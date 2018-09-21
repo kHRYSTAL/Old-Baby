5dc388a53c04a92c79c960de3ad19a7f5b202be4
kHRYSTAL <723526676@qq.com>
2018-09-21 12:26:59 +0800

    feat[oblib]:新增通用titlebar

---
+++ oblib/src/main/java/com/oldbaby/oblib/view/title/OnTitleClickListener.java
@@ -0,0 +1,14 @@
+package com.oldbaby.oblib.view.title;
+
+import android.view.View;
+
+/**
+ * usage: 点击标题栏按钮的事件
+ * author: kHRYSTAL
+ * create time: 18/9/21
+ * update time:
+ * email: 723526676@qq.com
+ */
+public interface OnTitleClickListener {
+    void onTitleClicked(View view, int tagId);
+}
