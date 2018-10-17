243a51c053e118c8026433713caa359d5a6e9a4c
kHRYSTAL <723526676@qq.com>
2018-10-17 15:05:09 +0800

    feat[tabhome]: 增加退出确认

--- app/src/main/java/com/oldbaby/tabhome/view/impl/TabHome.java
+++ app/src/main/java/com/oldbaby/tabhome/view/impl/TabHome.java
@@ -5,7 +5,9 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.oldbaby.R;
 import com.oldbaby.common.base.FragBaseActivity;
@@ -26,6 +28,7 @@ public class TabHome extends FragBaseActivity {
     private static final String TAG = TabHome.class.getSimpleName();
 
     FragTabHome fragTabHome;
+    private long mExitTime;
 
     @Override
     protected int titleType() {
@@ -81,6 +84,13 @@ public class TabHome extends FragBaseActivity {
         if (Jzvd.backPress()) {
             return;
         }
-        super.onBackPressed();
+        if ((System.currentTimeMillis() - mExitTime) > 2000) {
+            //大于2000ms则认为是误操作，使用Toast进行提示
+            Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            //并记录下本次点击“返回键”的时刻，以便下次进行判断
+            mExitTime = System.currentTimeMillis();
+        } else {
+            System.exit(0);
+        }
     }
 }
